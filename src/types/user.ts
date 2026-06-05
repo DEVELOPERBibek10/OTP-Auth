fb612a23c48695b0bbd11dd277cb6bd16e57b1a1
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-05 23:38:09 +0545

    add: extend User interface and UserSchema to include refreshToken and user methods

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,19 @@
 import type { Request } from "express";
+import type { Document } from "mongoose";
 export interface User extends Request {
   body: {
     username: string;
     email: string;
   };
+}
+
+interface UserMethods {
+  generateAccessToken(): string;
+  generateRefreshToken(): string;
+}
+
+export interface UserSchema extends Document, UserMethods {
+  username: string;
+  email: string;
+  refreshToken: string;
 }
