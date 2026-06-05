468bc7c94c8e2abf8d31b96579f42ef8f543a46a
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-05 23:36:15 +0545

    fix: update sendOTP method to handle number type for OTP and improve error handling

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -3,9 +3,10 @@ import mail from "../configs/mailer.js";
 import type { SentMessageInfo } from "nodemailer/lib/smtp-transport/index.js";
 import type { SMTPError } from "nodemailer/lib/smtp-connection/index.js";
 import { getOtpEmailTemplate } from "../templates/otpTemplate.js";
+import ApiError from "../utils/ApiError.js";
 
 class EmailService {
-  async sendOTP(email: string, otp: string): Promise<SentMessageInfo> {
+  async sendOTP(email: string, otp: number): Promise<SentMessageInfo> {
     const mailOptions: SendMailOptions = {
       from: `"App Security" <${process.env.GMAIL_USER}>`,
       to: email,
@@ -15,11 +16,23 @@ class EmailService {
     };
     try {
       const info = await mail.sendMail(mailOptions);
+
+      if (info.rejected.length > 0) {
+        throw new ApiError(
+          400,
+          "EMAIL_DELIVERY_FAILED",
+          `Email address ${email} was rejected by the provider.`
+        );
+      }
       return info;
     } catch (error) {
       const smtpError = error as SMTPError;
-      console.error("Email delivery failed:", smtpError.message);
-      throw smtpError;
+      console.error("Email delivery failed:", smtpError);
+      throw new ApiError(
+        smtpError.responseCode || 502,
+        smtpError.code || "EMAIL_SERVICE_ERROR",
+        smtpError.message
+      );
     }
   }
 }
