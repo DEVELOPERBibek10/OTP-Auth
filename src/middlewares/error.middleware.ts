5bedf84c65ddf2e7090272c0beb86f50b1eb60b4
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-07 14:13:09 +0545

    feat: implement global error handler with detailed error processing

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,3 +1,50 @@
-const globalErrorHandler = (err: Error, req: any, res: any, next: any) => {};
+import type { NextFunction, Request, Response } from "express";
+import mongoose, { Error as MongooseError } from "mongoose";
+
+import {
+  handleMongoDuplicateKey,
+  handleCastError,
+  handleParseError,
+} from "../utils/errorTransformers.js";
+import ApiError from "../utils/ApiError.js";
+import type { GlobalError } from "../types/error.js";
+
+const globalErrorHandler = (
+  err: GlobalError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let error = err;
+
+  if (err instanceof mongoose.mongo.MongoServerError && err.code === 11000)
+    error = handleMongoDuplicateKey(err);
+  else if (err instanceof MongooseError.CastError) error = handleCastError(err);
+  else if (
+    "body" in err &&
+    typeof err.status === "number" &&
+    err.type === "string" &&
+    err.type.includes(".")
+  )
+    error = handleParseError(err);
+
+  if (!(error instanceof ApiError)) {
+    console.error("CRITICAL SYSTEM ERROR:", error);
+    error = new ApiError(
+      500,
+      "INTERNAL_SERVER_ERROR",
+      error.message || "Internal Server Error"
+    );
+  }
+
+  res.status(error.statusCode).json({
+    success: false,
+    statusCode: error.statusCode,
+    code: error.code,
+    message: error.message,
+    errors: error.errors?.length ? error.errors : undefined,
+    stack: process.env.NODE_ENV === "development" ? error.stack : undefined,
+  });
+};
 
 export default globalErrorHandler;
