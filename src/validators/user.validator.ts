aa446aa1b3eef6fae597b2f509332abfec1e1565
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-08 16:27:04 +0545

    fix: update email validation in signUp and signIn schemas, and add verifySignInOrUp schema

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -6,8 +6,8 @@ export const signUpSchema = z.object({
       .email()
       .toLowerCase()
       .trim()
-      .min(1, { error: "Email is required." }),
-
+      .min(1, { error: "Email is required." })
+      .max(56, { error: "Invalid email address." }),
     username: z
       .string()
       .trim()
@@ -22,9 +22,26 @@ export const signInSchema = z.object({
       .email()
       .toLowerCase()
       .trim()
-      .min(1, { error: "Email is required." }),
+      .min(1, { error: "Email is required." })
+      .max(56, { error: "Invalid email address." }),
+  }),
+});
+
+export const verifySignInOrUpSchema = z.object({
+  body: z.object({
+    email: z
+      .email()
+      .toLowerCase()
+      .trim()
+      .min(1, { error: "Email is required." })
+      .max(56, { error: "Invalid email address." }),
+    otp: z.coerce
+      .number({ error: "Must be a valid number." })
+      .min(100000, { error: "6 digit OTP is expected." })
+      .max(999999, { error: "6 digit OTP is expected." }),
   }),
 });
 
 export type SignUpUser = z.infer<typeof signUpSchema>["body"];
 export type SignInUser = z.infer<typeof signInSchema>["body"];
+export type VerifySignInOrUp = z.infer<typeof verifySignInOrUpSchema>["body"];
