1775b1628f275f36d871c4433d0ff628b8a473d6
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-08 19:31:58 +0545

    fix: wrap getCurrentUser function with asyncHandler for error handling

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,13 @@
 import type { AuthRequest } from "../types/user.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import type { Response } from "express";
+import asyncHandler from "../utils/asyncHandler.js";
 
-export const getCurrentUser = async (req: AuthRequest, res: Response) => {
-  const user = req.user;
-  return res
-    .status(200)
-    .json(new ApiResponse(200, user, "User fetched sucessfully!"));
-};
+export const getCurrentUser = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const user = req.user;
+    return res
+      .status(200)
+      .json(new ApiResponse(200, user, "User fetched sucessfully!"));
+  }
+);
