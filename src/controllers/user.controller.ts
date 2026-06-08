a009c7ac27452a8dbb6550d7457734c65df068c7
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-08 16:22:14 +0545

    feat: add getCurrentUser controller for fetching current user details

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,13 @@
+import type { AuthRequest } from "../types/user.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import type { Response } from "express";
+
+export const getCurrentUser = asyncHandler(
+  async (req: AuthRequest, res: Response) => {
+    const user = req.user;
+    return res
+      .status(200)
+      .json(new ApiResponse(200, user, "User fetched sucessfully!"));
+  }
+);
