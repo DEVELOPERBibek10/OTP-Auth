c4155d53d313176278967d24a3ab88241c17f055
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-08 19:31:34 +0545

    fix: add routing for auth and user endpoints with global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 const app = express();
 
 app.use(
@@ -14,5 +15,13 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import { authRouter } from "./routes/auth.routes.js";
+import { userRouter } from "./routes/user.route.js";
+import globalErrorHandler from "./middlewares/error.middleware.js";
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/user", userRouter);
+app.use(globalErrorHandler);
 
 export default app;
