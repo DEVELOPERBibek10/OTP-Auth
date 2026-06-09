5525c6c0de31b236bfb1894f184baa737c5b08ae
DEVELOPERBibek10 <bibekpyakurel7@gmai.com>
2026-06-09 09:10:50 +0545

    fix: enhance request validation schema to include body type definition

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,15 +1,21 @@
-import type { ZodType } from "zod";
+import type { ZodObject, ZodType } from "zod";
 import asyncHandler from "../utils/asyncHandler.js";
 import type { Request, Response, NextFunction } from "express";
 import { ZodError } from "zod";
 import ApiError from "../utils/ApiError.js";
 
-export const validator = (schema: ZodType) =>
+type RequestSchema = ZodObject<{
+  body?: ZodType;
+}>;
+
+export const validator = (schema: RequestSchema) =>
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     try {
-      await schema.parseAsync({
+      const parsedData = await schema.parseAsync({
         body: req.body,
       });
+
+      req.body = parsedData.body;
       next();
     } catch (error) {
       if (error instanceof ZodError) {
